f3c4baabf10487070cc9df6af45faf77f1695f2f
KKevin0 <91940907+KKevin0@users.noreply.github.com>
2023-05-02 21:19:45 +0700

    create model customer
    MCustomer, MCustomerAdress, MCustomerPicture

--- app/Models/Customer/MCustomer.php
+++ app/Models/Customer/MCustomer.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class MCustomer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'customer_fullname',
+        'customer_nickname',
+        'customer_username',
+        'customer_telp',
+        'customer_gender',
+    ];
+
+    protected $primaryKey ='customer_id';
+
+    public function mCustomerAddress()
+    {
+        return $this->hasOne(MCustomerAddress::class,'customer_id','custumer_id');
+    }
+
+    public function mCustomerPicture()
+    {
+        return $this->hasOne(mCustomerPicture::class,'customer_id','custumer_id');
+    }
 }
--- app/Models/Customer/MCustormerPicture.php
+++ app/Models/Customer/MCustormerPicture.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class MCustormerPicture extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'picture_filename',
+        'picture_imagename',
+        'picture_type',
+        'picture_path',
+    ];
 }
