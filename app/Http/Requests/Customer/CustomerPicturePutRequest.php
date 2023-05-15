e741b7122c949ea507ed5cd64f94410c13d20fff
anugerahc <anugerahchristian@gmail.com>
2023-05-15 15:19:34 +0700

    add:changeCustomerPictureFeature

---
+++ app/Http/Requests/Customer/CustomerPicturePutRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Customer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerPicturePutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'customer_picture' => 'required|mimes:jpeg,jpg,png|min:30|max:10240',
+            "password" => "required",
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'customer_picture' => 'Foto Profile',
+            'password' => 'Kata Sandi'
+        ];
+    }
+}
