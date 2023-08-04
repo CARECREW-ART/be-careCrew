b09d76c9ccff72a5998f12adb77ec25eda44f0b1
anugerahc <anugerahchristian@gmail.com>
2023-08-04 10:12:43 +0700

    minor changes

--- resources/views/emails/reset_password_otp.blade.php
+++ resources/views/emails/reset_password_otp.blade.php
@@ -1,12 +1,9 @@
 <x-mail::message>
-    <p>Kepada, Bapak/Ibu</p>
-    <br>
+    <p>Kepada, Bapak/Ibu<br></p>
 
     <p>Gunakan Kode OTP berikut untuk mengatur ulang kata sandi anda:</p>
     <p><strong>{{ $otpCode }}</strong></p>
-    <p>Kode akan kadaluwarsa dalam 10 menit</p>
-    <br>
-    <br>
+    <p>Kode akan kadaluwarsa dalam 10 menit<br><br></p>
 
     <p>Jika ini bukan Anda, abaikan email ini. Mohon untuk tidak membalas email ini</p>
 
